5ff7c00819f9d326735bc55af687092e3b164e59
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-01-21 13:53:25 +0530

    Add hyde file

---
+++ .hyde.el
@@ -0,0 +1,8 @@
+(setq hyde/git/remote "origin"
+      hyde-deploy-dir "public"
+      hyde-posts-dir "source/_posts"
+      hyde-drafts-dir "source/_drafts"
+      hyde/deploy-command "rvm ruby-1.9.2-p290 exec rake rsync"
+      hyde/jekyll-command "rvm ruby-1.9.2-p290 exec rake generate"
+)
+
