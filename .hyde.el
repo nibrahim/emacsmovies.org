4cf263e510a1391c09168de3428ccc1305ef1ed8
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-07-17 19:31:38 +0530

    Fixed deploy command.

--- .hyde.el
+++ .hyde.el
@@ -2,7 +2,7 @@
       hyde-deploy-dir "public"
       hyde-posts-dir "source/_posts"
       hyde-drafts-dir "source/_drafts"
-      hyde/deploy-command "rvm ruby-1.9.2-p290 exec bundle exec rake rsync"
-      hyde/jekyll-command "rvm ruby-1.9.2-p290 exec bundle exec rake generate"
+      hyde/deploy-command "rake rsync"
+      hyde/jekyll-command "rake generate"
 )
 
